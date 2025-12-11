29d463e705f80c1b0caca3a6b9ea015e49992732
minchey <nolam@naver.com>
2025-12-12 01:26:24 +0900

    runtime 폴더 재생성 및 bat 파일 수정

--- run.bat
+++ run.bat
@@ -1,15 +1,10 @@
 @echo off
+cd /d "%~dp0"
+
 echo ============================================
 echo   Simple IDS 실행중...
-echo   (Npcap이 설치되어 있어야 합니다)
 echo ============================================
-echo.
 
-runtime\bin\java -jar untitled-1.0-SNAPSHOT-all.jar
+runtime\bin\java -jar build\libs\simple-ids.jar
 
-echo.
-echo --------------------------------------------
-echo   프로그램이 종료되었습니다.
-echo   창을 닫으려면 아무 키나 누르세요...
-echo --------------------------------------------
-pause > nul
+pause
